9567fdd42c8efe8ad2066409ec6c8b8639af1b95
kikorose <nakelia78@gmil.com>
2017-08-22 15:46:20 -0400

    first commit(complete data life cycle)

---
+++ src/main/java/com/example/demo/DemoSpringBoot10Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot10Application.class, args);
+	}
+}
